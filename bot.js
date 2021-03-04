9bee86179ca662d38c8b656711da10f3f504c438
Michael Medvediev <misha.medvedev2001@gmail.com>
2021-03-04 18:11:44 +0200

    Bot released

---
+++ bot.js
@@ -0,0 +1,14 @@
+const TelegramBot = require('node-telegram-bot-api')
+require('dotenv').config()
+
+const token = process.env.TOKEN
+const bot = new TelegramBot(token, {
+    polling: true
+})
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const firstLetters = msg.text.slice(0, 3).toLowerCase();
+    
+    if (firstLetters === 'git' || firstLetters === 'гит')
+        bot.sendMessage(chatId, 'КТО-ТО СКАЗАЛ УРОКИ ПО ГИТУ??? @medved2001');
+});
