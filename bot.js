0bd4c6b2cabaee9479d74a90c6f25008b8a1c6d3
Michael Medvediev <misha.medvedev2001@gmail.com>
2021-03-04 18:19:46 +0200

    Some debugging

--- bot.js
+++ bot.js
@@ -5,10 +5,17 @@ const token = process.env.TOKEN
 const bot = new TelegramBot(token, {
     polling: true
 })
+
+bot.on("polling_error", (msg) => console.log(msg));
+
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    const firstLetters = msg.text.slice(0, 3).toLowerCase();
-    
-    if (firstLetters === 'git' || firstLetters === 'гит')
-        bot.sendMessage(chatId, 'КТО-ТО СКАЗАЛ УРОКИ ПО ГИТУ??? @medved2001');
+    try {
+        const chatId = msg.chat.id;
+        const firstLetters = msg.text.slice(0, 3).toLowerCase();
+
+        if (firstLetters === 'git' || firstLetters === 'гит')
+            bot.sendMessage(chatId, 'КТО-ТО СКАЗАЛ УРОКИ ПО ГИТУ??? @medved2001');
+    } catch (error) {
+        console.log(error)
+    }
 });
