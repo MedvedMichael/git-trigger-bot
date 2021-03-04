c690344960a7d5deba93c0e84b896f4e8fd9a424
Michael Medvediev <misha.medvedev2001@gmail.com>
2021-03-04 18:26:10 +0200

    Some debugging

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api')
-// require('dotenv').config()
+require('dotenv').config()
 
 const token = process.env.TOKEN
 const bot = new TelegramBot(token, {
@@ -8,13 +8,18 @@ const bot = new TelegramBot(token, {
 
 bot.on("polling_error", (msg) => console.log(msg));
 
-bot.on('message', (msg) => {
+bot.on('message', ({ chat, text }) => {
     try {
-        const chatId = msg.chat.id;
-        const firstLetters = msg.text.slice(0, 3).toLowerCase();
+        const chatId = chat.id;
+        const words = text.split(' ')
+        for (let index = 0; index < words.length; index++) {
+            const element = words[index];
+            const firstLetters = element.slice(0, 3).toLowerCase();
 
-        if (firstLetters === 'git' || firstLetters === 'гит')
-            bot.sendMessage(chatId, 'КТО-ТО СКАЗАЛ УРОКИ ПО ГИТУ??? @medved2001');
+            if (firstLetters === 'git' || firstLetters === 'гит' || (firstLetters === 'пше' && element.length === 3))
+                bot.sendMessage(chatId, 'КТО-ТО СКАЗАЛ УРОКИ ПО ГИТУ??? @medved2001');
+        }
+
     } catch (error) {
         console.log(error)
     }
